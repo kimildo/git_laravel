dee43ec3c461be3a473ee57da067f07d87cbbc10
kimildo <kimildo78@gmail.com>
2017-02-07 11:12:07 +0900

    master commit

--- routes/web.php
+++ routes/web.php
@@ -1,24 +1,8 @@
 <?php
 
-    /*
-    |--------------------------------------------------------------------------
-    | Web Routes
-    |--------------------------------------------------------------------------
-    |
-    | Here is where you can register web routes for your application. These
-    | routes are loaded by the RouteServiceProvider within a group which
-    | contains the "web" middleware group. Now create something great!
-    |
-    */
-
-    /*Route::get('/', function () {
-        return view('welcome');
-    });
-
-    Route::get('/home', function () {
-        return view('home');
-    });*/
-
+    /**
+     * add this from master
+     */
     trigger_error('master');
     trigger_error('master');
     trigger_error('master');
