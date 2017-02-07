d634bc8c3431f80e83b2b1ab6ce5ac2702d3cccd
kimildo <kimildo78@gmail.com>
2017-02-07 12:04:20 +0900

    master commit

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -1,31 +1,31 @@
 <?php
 
-namespace App\Http\Controllers;
+    namespace App\Http\Controllers;
 
-class WelcomeController extends Controller
-{
-    /**
-     * Say hello to visitors.
-     *
-     * @return \Illuminate\Contracts\View\Factory
-     */
-    public function index() {
-        return view('welcome');
-    }
-
-    /**
-     * Set locale.
-     *
-     * @return \Illuminate\Http\RedirectResponse
-     */
-    public function locale()
+    class WelcomeController extends Controller
     {
-        $cookie = cookie()->forever('locale__myapp', request('locale'));
+        /**
+         * Say hello to visitors.
+         *
+         * @return \Illuminate\Contracts\View\Factory
+         */
+        public function index() {
+            return view('welcome');
+        }
 
-        cookie()->queue($cookie);
+        /**
+         * Set locale.
+         *
+         * @return \Illuminate\Http\RedirectResponse
+         */
+        public function locale()
+        {
+            $cookie = cookie()->forever('locale__myapp', request('locale'));
 
-        return ($return = request('return'))
-            ? redirect(urldecode($return))->withCookie($cookie)
-            : redirect('/')->withCookie($cookie);
+            cookie()->queue($cookie);
+
+            return ($return = request('return'))
+                ? redirect(urldecode($return))->withCookie($cookie)
+                : redirect('/')->withCookie($cookie);
+        }
     }
-}
